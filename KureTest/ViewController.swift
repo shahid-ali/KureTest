1cf5001249294e24288e2b9e56f59de982934f89
shahid-ali <shahid.ali298@gmail.com>
2021-07-23 19:17:00 +0530

    Initial Commit

---
+++ KureTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KureTest
+//
+//  Created by Shahid Ali on 7/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
